d8768e4912055c6f0fb17a5d59137192cb4ceebe
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2026-08-30 17:34:07 -0300

    commit

--- Python/Atividade/ex.py
+++ Python/Atividade/ex.py
@@ -1,1 +1,19 @@
-idade = 
+idade = int(input("Digite sua idade: "))
+if idade < 18:
+    print("Emprestimo não permitido para menores de idade.")
+else:
+    salario = float(input("Digite seu salário: "))
+    emprestimo = float(input("Digite o valor do empréstimo desejado: "))
+    
+    limite30 = salario * 0.3 
+    limite50 = salario * 0.5 
+    
+    if emprestimo <= limite30:
+        print("Empréstimo aprovado.")
+    elif emprestimo <= limite50:
+        print("Analise manual, em breve entraremos em contato.")
+    else:
+        print("Empréstimo não permitido. O valor solicitado excede o limite permitido.")
+
+        
+    #print(f"Limite de empréstimo permitido: R${limite:.2f}")
