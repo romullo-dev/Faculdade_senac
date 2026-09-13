9ba0daac81140fdf18a0828bad9072bebf89929b
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2026-09-12 21:40:24 -0300

    c

--- Javascript/Atividades/Atividade-03/Exercício_7/script.js
+++ Javascript/Atividades/Atividade-03/Exercício_7/script.js
@@ -1,6 +1,6 @@
 let nome_cliente = prompt("Digite o nome do cliente: ");
 let valor_compra = parseFloat(prompt("Digite o valor da compra: "));
-let regiao_entrega = prompt("Digite a região de entrega (Norte, Sul, Leste ou Oeste): ");
+let regiao_entrega = prompt("Digite a região de entrega (Norte, Sul, Leste ou Oeste): ").toLowerCase;
 let cumpom_informado = prompt("Digite o cupom de desconto (se houver): ");
 let forma_pagamento = prompt("Digite a forma de pagamento (dinheiro, cartão ou pix): ").toLowerCase();
 let valor_frete = 0; 
@@ -20,4 +20,6 @@ switch (regiao_entrega.toLowerCase()) {
         alert("A região escolhida não é atendida");
 }
 
-alert(forma_pagamento)
+if (forma_pagamento == "pix") {
+    
+}
