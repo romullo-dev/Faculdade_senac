54e811995d2af52f6c02ff68ff5c4b569fb3719f
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2026-09-13 12:55:41 -0300

    commit

--- Javascript/Atividades/Atividade-03/Exercício_7/script.js
+++ Javascript/Atividades/Atividade-03/Exercício_7/script.js
@@ -1,25 +1,51 @@
 let nome_cliente = prompt("Digite o nome do cliente: ");
 let valor_compra = parseFloat(prompt("Digite o valor da compra: "));
-let regiao_entrega = prompt("Digite a região de entrega (Norte, Sul, Leste ou Oeste): ").toLowerCase;
-let cumpom_informado = prompt("Digite o cupom de desconto (se houver): ");
+let regiao_entrega = prompt("Digite a região de entrega (Norte, Sul, Leste ou Oeste): ").toLowerCase();
+let cumpom_informado = prompt("Digite o cupom de desconto (se houver): ").toLowerCase();
 let forma_pagamento = prompt("Digite a forma de pagamento (dinheiro, cartão ou pix): ").toLowerCase();
 let valor_frete = 0; 
+let desconto_pix = 0;
+let desconto_cupom = 0;
+let valor_total = 0
 
 
-switch (regiao_entrega.toLowerCase()) {
+switch (regiao_entrega) {
     case "norte":
-        valor_frete += 20;
+        if (forma_pagamento > 250) {
+            valor_frete = 0;
+        } else{
+            valor_frete += 20;
+        }
         break;
     case "sul":
-        valor_frete += 15;
+        if (forma_pagamento > 250) {
+            valor_frete = 0;
+        } else{
+            valor_frete += 15;
+        }
         break;
     case "centro":
-        valor_frete += 10;
+        if (forma_pagamento > 250) {
+            valor_frete = 0;
+        } else{
+            valor_frete += 10;
+        }
         break;
     default:
         alert("A região escolhida não é atendida");
 }
 
-if (forma_pagamento == "pix") {
-    
+if (cumpom_informado == "ads10" && valor_compra >= 100 ) {
+    desconto_pix = (valor_compra * 0.10)
 }
+
+if (forma_pagamento == "pix") {
+    desconto_cupom = (valor_compra * 0.05)
+}
+
+valor_total = 
+
+
+
+
+
