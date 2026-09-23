ee75790cb5da61b11fbceaeb09f115ab0b3101d8
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2026-09-23 19:33:27 -0300

    commit

---
+++ Python/Aula_07_23_09/ex1 copy.py
@@ -0,0 +1,6 @@
+cidades = ["brasilia", "goiania", "brasilia","palmas","goiania"]
+cidades_unicas = set(cidades)
+
+print(cidades_unicas)
+
+print("Quantidade: ", len(cidades_unicas))
---
+++ Python/Aula_07_23_09/ex1.py
@@ -0,0 +1,9 @@
+acessos={"Ana","Predro","Beatriz","João"}
+
+print(acessos)
+print("Tamanho: ", len(acessos))
+
+acessos.add("Paulo")
+acessos.remove("Ana")
+
+print(acessos)
