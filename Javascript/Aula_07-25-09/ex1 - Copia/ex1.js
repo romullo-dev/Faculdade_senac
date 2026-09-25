67148fd35d808e1c687cb54d9ef335103821f516
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2026-09-25 20:23:18 -0300

    c

---
+++ Javascript/Aula_07-25-09/ex1 - Copia/ex1.js
@@ -0,0 +1,21 @@
+const livro = {
+    titulo : prompt("Digite o nome do titulo "),
+    n_pag : prompt("Digite o numero de pagina "),
+    autor : prompt("Digite o nome do autor ")
+};
+
+const livro2 = {
+    titulo : prompt("Digite o nome do titulo "),
+    n_pag : prompt("Digite o numero de pagina "),
+    autor : prompt("Digite o nome do autor ")
+};
+
+alert(livro.titulo +
+    livro.n_pag + 
+    livro.autor
+);
+
+alert(livro2.titulo +
+    livro2.n_pag + 
+    livro2.autor
+);
